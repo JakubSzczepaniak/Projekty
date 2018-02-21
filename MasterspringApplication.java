26ee6e8c3874ccd9da148ab8508dab6c123b9259
kuba123 <jakub_89@wp.pl>
2018-02-21 17:22:46 +0100

    Add files via upload

---
+++ MasterspringApplication.java
@@ -0,0 +1,12 @@
+package pl.masterspringmvc4m.masterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterspringApplication.class, args);
+	}
+}
